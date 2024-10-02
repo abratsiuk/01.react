616ae04f247d07636b3f3f48c072c62980e5af49
Aleksandr Bratsiuk <bratsiuk@outlook.com>
2024-10-02 10:59:08 +0300

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div>
-      hello world
-    </div>
-  );
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            count: 0,
+        };
+        this.handleIncrease = this.handleIncrease.bind(this);
+        this.handleDecrease = this.handleDecrease.bind(this);
+    }
+
+    handleIncrease() {
+        this.setState({ count: this.state.count + 1 });
+    };
+    handleDecrease() {
+      this.setState({ count: this.state.count - 1 });
+  };    
+
+    render() {
+        return (
+            <div className="App">
+                <button onClick={this.handleIncrease}>+</button>
+                <span>{this.state.count}</span>
+                <button onClick={this.handleDecrease}>-</button>
+            </div>
+        );
+    }
 }
 
 export default App;
