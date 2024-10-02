c02bba434e380da44a4fa97f752ef0499041e707
Aleksandr Bratsiuk <bratsiuk@outlook.com>
2024-10-02 11:01:10 +0300

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            count: 0,
-        };
-        this.handleIncrease = this.handleIncrease.bind(this);
-        this.handleDecrease = this.handleDecrease.bind(this);
-    }
-
-    handleIncrease() {
-        this.setState({ count: this.state.count + 1 });
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
     };
-    handleDecrease() {
-      this.setState({ count: this.state.count - 1 });
-  };    
+    this.handleIncrease = this.handleIncrease.bind(this);
+    this.handleDecrease = this.handleDecrease.bind(this);
+  }
 
-    render() {
-        return (
-            <div className="App">
-                <button onClick={this.handleIncrease}>+</button>
-                <span>{this.state.count}</span>
-                <button onClick={this.handleDecrease}>-</button>
-            </div>
-        );
-    }
+  handleIncrease() {
+    this.setState({ count: this.state.count + 1 });
+  }
+  handleDecrease() {
+    this.setState({ count: this.state.count - 1 });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.handleDecrease}>-</button>
+        <span>{this.state.count}</span>
+        <button onClick={this.handleIncrease}>+</button>
+      </div>
+    );
+  }
 }
 
 export default App;
