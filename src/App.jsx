da215aadc8778b53cdca5b08ec1dcea4e7a326ba
Aleksandr Bratsiuk <bratsiuk@outlook.com>
2024-10-02 10:54:29 +0300

    change

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,26 @@ class App extends Component {
     super(props);
     this.state = { count: 0 };
 
-    this.increase = this.increase.bind(this);
-    this.decrease = this.decrease.bind(this);
+    // this.increase = this.increase.bind(this);
+    // this.decrease = this.decrease.bind(this);
   }
 
-  increase(){
-    this.setState({count:this.state.count+1})
-  }
-  
-  decrease(){
-    this.setState({count:this.state.count-1})
-  }
+  increase = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  decrease = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
 
   render() {
-    return (<div>
-      <span>This state: </span>
-      <span>{this.state.count} </span>
-      <button onClick={this.increase}> + </button>
-      <button onClick={this.decrease}> - </button>
-      </div>);
+    return (
+      <div>
+        <button onClick={this.increase}> + </button>
+        <span> {this.state.count} </span>
+        <button onClick={this.decrease}> - </button>
+      </div>
+    );
   }
 }
 
