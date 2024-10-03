4d8aea9706816995b634243a678c2bf7f7ecf681
Aleksandr Bratsiuk <bratsiuk@outlook.com>
2024-10-03 13:19:58 +0300

    finish

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,8 +5,7 @@ import pluginReact from "eslint-plugin-react";
 
 export default [
   {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
-  { settings: { react: { version: 'detect' },},}
 ];
