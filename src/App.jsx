be5f501e3b6752114ffd0fdafb2281e914bd9d71
Aleksandr Bratsiuk <bratsiuk@outlook.com>
2024-10-15 17:55:15 +0300

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { Component } from 'react';
 
-function App() {
-  
-  return (
-    <div>
-      hello world
-    </div>
-  );
+class App extends Component {
+    state = {
+        count: 0,
+    };
+
+    increment = () => {
+        this.setState({ count: this.state.count + 1 });
+    };
+
+    decrement = () => {
+        this.setState({ count: this.state.count - 1 });
+    };
+
+    render() {
+        return (
+            <div className='App'>
+                <button onClick={this.decrement}>-</button>
+                <span>{this.state.count}</span>
+                <button onClick={this.increment}>+</button>
+            </div>
+        );
+    }
 }
 
 export default App;
