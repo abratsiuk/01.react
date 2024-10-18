1e3f16cece13fbb176b6aa3f925c121ac3a01c21
Aleksandr Bratsiuk <bratsiuk@outlook.com>
2024-10-18 12:00:59 +0300

    fixed

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -6,62 +6,38 @@ function Timer() {
     let counterId = useRef(null);
 
     const handleStart = () => {
-        console.log(
-            `start, count: ${count}, isCounting: ${isCounting}, counterId: ${counterId.current}`
-        );
         setIsCounting(true);
-
         counterId.current = setInterval(() => {
             setCount((prev) => prev + 1);
         }, 1000);
-        console.log(`->->set interval, counterId: ${counterId.current}`);
     };
 
     const handleStop = () => {
-        console.log(
-            `stop, count: ${count}, isCounting: ${isCounting}, counterId: ${counterId.current}`
-        );
-
         if (counterId.current) {
             clearInterval(counterId.current);
             counterId.current = null;
             console.log(`-|-|clear interval, counterId: ${counterId.current}`);
         }
-
         setIsCounting(false);
     };
 
     const handleReset = () => {
-        console.log(
-            `reset, count: ${count}, isCounting: ${isCounting}, counterId: ${counterId.current}`
-        );
-
         handleStop();
         setCount(0);
     };
 
     useEffect(() => {
         const userCount = localStorage.getItem('count');
-        console.log(
-            `useEffect - mounting, load from storage, userCount: ${userCount}`
-        );
-
         if (userCount) setCount(+userCount);
+
         return () => {
-            console.log(`_____useEffect - unmounting`);
             if (counterId.current) {
                 clearInterval(counterId.current);
-                console.log(
-                    `-|-|clear interval, counterId: ${counterId.current}`
-                );
             }
         };
     }, []);
 
     useEffect(() => {
-        console.log(
-            `useEffect - change count and write to localStorage, count: ${count}`
-        );
         localStorage.setItem('count', count);
     }, [count]);
 
