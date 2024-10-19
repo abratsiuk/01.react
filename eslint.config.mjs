3e59dd6629a7b5eca5c991905ca6692189bab0ee
Aleksandr Bratsiuk <bratsiuk@outlook.com>
2024-10-19 16:01:47 +0300

    chore: update ESLint configuration and dependencies to include react-hooks

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,20 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import pluginReact from 'eslint-plugin-react';
+import pluginReactHooks from 'eslint-plugin-react-hooks';
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
+    { files: ['**/*.{js,mjs,cjs,jsx}'] },
+    { languageOptions: { globals: globals.browser } },
+    pluginJs.configs.recommended,
+    pluginReact.configs.flat.recommended,
+    {
+        plugins: {
+            reactHooks: pluginReactHooks,
+        },
+        rules: {
+            'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+            'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
+        },
+    },
 ];
